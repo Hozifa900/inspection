349f26b3a658a76c61fd9912a8bb7fc95321e3f5
Hozifa Zeanalabdeen <pundit1d@gmail.com>
2024-02-10 18:49:35 -0500

    add test file to test monitor script

---
+++ newfront/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import GiveList from "./Pages/GiveList/GiveList";
+import Hero from "./Pages/Hero/Hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <GiveList />
+    </>
+  );
+}
+
+export default App;
